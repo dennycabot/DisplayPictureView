b217f0ff8162996c7e7258ef865a4f39c9492fe1
Denny Mathew <dennymathew@cs-058-dt-mac.local>
2017-02-21 14:29:39 +0530

    Initial Commit

---
+++ DisplayPictureView/DisplayPictureView.h
@@ -0,0 +1,19 @@
+//
+//  DisplayPictureView.h
+//  DisplayPictureView
+//
+//  Created by Denny Mathew on 21/02/17.
+//  Copyright © 2017 Cabot Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DisplayPictureView.
+FOUNDATION_EXPORT double DisplayPictureViewVersionNumber;
+
+//! Project version string for DisplayPictureView.
+FOUNDATION_EXPORT const unsigned char DisplayPictureViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DisplayPictureView/PublicHeader.h>
+
+
